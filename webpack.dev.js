2bd43f4871550a17fa57cc53240f72654e390d70
Igor Propisnov <i.propisnov@dobosolution.de>
2019-12-03 19:00:27 +0100

    Added webpack rule for css

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,30 +2,43 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 
-  // https://webpack.js.org/concepts/entry-points/#multi-page-application
-  entry: {
-    index: './src/index.js',
-    demo: './src/demo.js'
-  },
+    // https://webpack.js.org/concepts/entry-points/#multi-page-application
+    entry: {
+        index: './src/index.js',
+        demo: './src/demo.js'
+    },
 
-  // https://webpack.js.org/configuration/dev-server/
-  devServer: {
-    port: 8080
-  },
+    // https://webpack.js.org/configuration/dev-server/
+    devServer: {
+        port: 8080
+    },
+
+    // https://webpack.js.org/concepts/loaders/
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            }
+        ]
+    },
 
   // https://webpack.js.org/concepts/plugins/
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      inject: true,
-      chunks: ['index'],
-      filename: 'index.html'
-    }),
-    new HtmlWebpackPlugin({
-        template: './src/demo.html',
-        inject: true,
-        chunks: ['demo'],
-        filename: 'demo.html'
-      })
-  ]
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            inject: true,
+            chunks: ['index'],
+            filename: 'index.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/demo.html',
+            inject: true,
+            chunks: ['demo'],
+            filename: 'demo.html'
+        })
+    ]
 };
