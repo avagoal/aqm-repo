56dc02e431737c24c8048aef6223e207ea540050
Igor Propisnov <i.propisnov@dobosolution.de>
2019-12-03 18:55:05 +0100

    Configure webpack for demo files

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,7 +4,8 @@ module.exports = {
 
   // https://webpack.js.org/concepts/entry-points/#multi-page-application
   entry: {
-    index: './src/index.js'
+    index: './src/index.js',
+    demo: './src/demo.js'
   },
 
   // https://webpack.js.org/configuration/dev-server/
@@ -19,6 +20,12 @@ module.exports = {
       inject: true,
       chunks: ['index'],
       filename: 'index.html'
-    })
+    }),
+    new HtmlWebpackPlugin({
+        template: './src/demo.html',
+        inject: true,
+        chunks: ['demo'],
+        filename: 'demo.html'
+      })
   ]
 };
